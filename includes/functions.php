46f4638d57698e668fb6980828325ebfb7e30e9d
Costa Gauci <costagauci@gmail.com>
2020-10-23 09:21:36 +0100

    Update functions.php
    New user confirmed still showing user_nicename not user_login.

--- includes/functions.php
+++ includes/functions.php
@@ -539,7 +539,7 @@ function uv_resend_verification_form($attr){
                     '{last_name}' => esc_html($user_data->last_name),
                     '{user_display_name}' => esc_html($user_data->display_name),
                     '{user_email}' => esc_html($user_data->user_email),
-                    '{user_name}' => esc_html($user_data->user_nicename),
+                    '{user_name}' => esc_html($user_data->user_login),
                     '{user_avatar}' => get_avatar( $user_data->user_email, 60 ),
 
                     '{ac_activaton_url}' => esc_url_raw($verification_url),
