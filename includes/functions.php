a53c1688bf2462a11de99444fc17dfaf20d95544
PickPlugins <support@pickplugins.com>
2020-11-21 09:11:32 +0600

    u

--- includes/functions.php
+++ includes/functions.php
@@ -838,7 +838,7 @@ if ( ! function_exists( 'user_verification_user_registered' ) ) {
             '{last_name}' => esc_html($user_data->last_name),
             '{user_display_name}' => esc_html($user_data->display_name),
             '{user_email}' => esc_html($user_data->user_email),
-            '{user_name}' => esc_html($user_data->user_login),
+            '{user_name}' => esc_html($user_data->user_nicename),
             '{user_avatar}' => get_avatar( $user_data->user_email, 60 ),
 
             '{ac_activaton_url}' => esc_url_raw($verification_url),
