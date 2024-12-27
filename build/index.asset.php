b8886454343556286a7483289b793b9a08b1c6c7
Nur <support@pickplugins.com>
2024-12-27 14:09:10 +0600

    update version

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => '6ad8a9a6f64b190269fa');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => '22594b9f2432e5adbd16');
--- src/components/input-textarea/index.js
+++ src/components/input-textarea/index.js
@@ -1,14 +1,75 @@
 const { Component } = wp.element;
 
+import { useEffect, useState } from "@wordpress/element";
+
 function Html(props) {
 	if (!props.warn) {
 		return null;
 	}
 
+	const [content, setContent] = useState('');
+
+	console.log(props.id);
+
+
+	useEffect(() => {
+		//tinymce.execCommand('mceAddEditor', true, props.id);
+
+		// console.log(props.id);
+
+		// wp.editor.initialize(props.id, {
+		//   tinymce: {
+		//     wpautop: true,
+		//     toolbar1:
+		//       "bold italic underline strikethrough | bullist numlist | blockquote hr wp_more | alignleft aligncenter alignright | link unlink | fullscreen | wp_adv",
+		//     toolbar2:
+		//       "formatselect alignjustify forecolor | pastetext removeformat charmap | outdent indent | undo redo | wp_help",
+		//   },
+		//   quicktags: true,
+		//   mediaButtons: true,
+		// });
+
+
+		// Function to capture content change
+		// const updateContent = () => {
+		//   const newContent = wp.editor.getContent(props.id);
+
+		//   console.log(newContent);
+
+
+		//   setContent(newContent);
+		// };
+
+		// // Listen for changes in the content
+		// document.getElementById(props.id).addEventListener('input', updateContent);
+
+		// Cleanup on unmount
+		// return () => {
+		//   document.getElementById(props.id).removeEventListener('input', updateContent);
+		// };
+
+
+		tinymce.init({
+			selector: "#" + props.id, toolbar:
+				"undo redo print spellcheckdialog formatpainter | blocks fontfamily fontsize | bold italic underline forecolor backcolor | link image | alignleft aligncenter alignright alignjustify lineheight | checklist bullist numlist indent outdent | removeformat", height: "500px", setup: (editor) => {
+					editor.on("change", (e) => {
+						const newContent = editor.getContent(); // Get the updated content
+						console.log(newContent); props.onChange(newContent);
+					});
+				},
+		});
+	}, []);
+
+
+
+
+
 	return (
 		<textarea
 			className={props.className}
 			id={props.id}
+			value={props.value}
+			type="text"
 			size={props.size}
 			name={props.name}
 			placeholder={props.placeholder}
@@ -16,10 +77,9 @@ function Html(props) {
 			maxlength={props.maxlength}
 			required={props.required}
 			disabled={props.disabled}
-			onChange={(newVal) => {
-				props.onChange(newVal);
-			}}
-		>
+			onChange={(e) => {
+				props.onChange(e.target.value);
+			}}>
 			{props.value}
 		</textarea>
 	);
@@ -28,7 +88,9 @@ function Html(props) {
 class PGinputTextarea extends Component {
 	constructor(props) {
 		super(props);
+
 		this.state = { showWarning: true };
+
 		this.handleToggleClick = this.handleToggleClick.bind(this);
 	}
 
@@ -40,17 +102,7 @@ class PGinputTextarea extends Component {
 
 	render() {
 		var {
-			value,
-			placeholder,
-			className,
-			id,
-			name,
-			size,
-			minlength,
-			maxlength,
-			required,
-			disabled,
-			onChange,
+			value, placeholder, className, id, name, size, minlength, maxlength, required, disabled, onChange,
 		} = this.props;
 
 		return (
